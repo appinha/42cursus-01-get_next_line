01103149db479200b5e6b52c957c6d2d980f8e00
appinha <amanda_pinha@hotmail.com>
2020-04-18 19:14:09 -0300

    Code reviewed v2 (ft_buff2line completely rewritten)

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,13 @@
 /*   By: apuchill <apuchill@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/29 20:01:42 by apuchill          #+#    #+#             */
-/*   Updated: 2020/04/18 14:37:48 by apuchill         ###   ########.fr       */
+/*   Updated: 2020/04/18 19:08:16 by apuchill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static size_t	ft_strchr_i(const char *s, int c)
-{
-	size_t i;
-
-	i = 0;
-	while (s[i] != '\0' && c != s[i])
-		i++;
-	if (c == s[i])
-		return (i);
-	return (-1);
-}
-
-static void		ft_free_nl(char **ptr)
+static void		ft_free_null(char **ptr)
 {
 	if (!ptr || !*ptr)
 		return ;
@@ -32,33 +20,54 @@ static void		ft_free_nl(char **ptr)
 	*ptr = NULL;
 }
 
-static int		ft_buff2line(char **line, char **buff)
+static void	ft_cpy_exc_buff(char **buff, long long j)
 {
-	long long	i_pos;
-	char		*tmp;
+	long long	i;
+	char		tmp[ARG_MAX];
 
-	tmp = ft_strjoin(*line, *buff);
-	ft_free_nl(&*line);
-	ft_free_nl(&*buff);
-	if ((i_pos = ft_strchr_i(tmp, '\n')) > -1)
-	{
-		*line = ft_substr(tmp, 0, i_pos);
-		if((ft_strlen(tmp) - i_pos) > 1)
-			*buff = ft_substr(tmp, i_pos + 1 , (ft_strlen(tmp) - i_pos - 1));
-		ft_free_nl(&tmp);
-		return (1);
-	}
+	i = 0;
+	if (buff[0][j] == '\0')
+		ft_free_null(&*buff);
 	else
 	{
-		*line = ft_strdup(tmp);
-		ft_free_nl(&tmp);
-		return (0);
+		while (buff[0][j] != '\0')
+			tmp[i++] = buff[0][j++];
+		tmp[i] = '\0';
+		ft_free_null(&*buff);
+		*buff = ft_strdup(tmp);
 	}
+}
+
+static int		ft_buff2line(char **line, char **buff)
+{
+	long long	i;
+	long long	j;
+	char		tmp[ARG_MAX];
+
+	i = 0;
+	j = 0;
+	while (line[0] != NULL && line[0][i] != '\0')
+	{
+		tmp[i] = line[0][i];
+		i++;
+	}
+	ft_free_null(&*line);
+	while (buff[0][j] != '\0' && buff[0][j] != '\n')
+		tmp[i++] = buff[0][j++];
+	tmp[i] = 0;
+	*line = ft_strdup(tmp);
+	if (buff[0][j] == '\n')
+	{
+		ft_cpy_exc_buff(&*buff, j + 1);
+		return (1);
+	}
+	ft_free_null(&*buff);
+	return (0);
 }
 
 int				get_next_line(int fd, char **line)
 {
-	static char	*buff[FOPEN_MAX];
+	static char	*buff[OPEN_MAX];
 	int			read_ret;
 	int			status;
 
@@ -67,11 +76,10 @@ int				get_next_line(int fd, char **line)
 		status = 0;
 		while (status == 0)
 		{
-			if (buff[fd] == NULL && (buff[fd] = malloc(BUFFER_SIZE + 1)))
+			if (buff[fd] == NULL && (buff[fd] = ft_calloc(BUFFER_SIZE + 1, 1)))
 				read_ret = read(fd, buff[fd], BUFFER_SIZE);
 			else
 				read_ret = ft_strlen(buff[fd]);
-			buff[fd][read_ret] = 0;
 			status = ft_buff2line(&*line, &buff[fd]);
 			if (read_ret == 0)
 				return (EOF_RCHD);
@@ -81,6 +89,6 @@ int				get_next_line(int fd, char **line)
 		if (status == 1)
 			return (READL_OK);
 	}
-	ft_free_nl(&*line);
+	ft_free_null(&*line);
 	return (ERR_HPND);
 }
